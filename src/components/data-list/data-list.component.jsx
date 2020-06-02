8e7ec90b942e35f3717beef7782d54f8e8d8969d
Cassandra Rossall <cassandrarossall@gmail.com>
2020-06-02 16:05:37 +1200

    added function to check for errors when receiving data from api -- null values were causing app to crash

--- src/components/data-list/data-list.component.jsx
+++ src/components/data-list/data-list.component.jsx
@@ -8,23 +8,38 @@ const DataList = ({ filteredData }) => {
 
     return (
     <Fragment>
-        {filteredData.map((data, index) => 
-        <ListContainer key = {index}>
+        {filteredData.map((data, index) => {
+          const getData = keyword => {
+            if(data !== null)
+            {
+              try {
+                return data[keyword].toLocaleString()
+              }
+              catch (error) {
+                console.log(error)
+              }
+            }
+          }
+
+          return (
+          <ListContainer key = {index}>
             <h2>{data.country.toUpperCase()}</h2>
               <div className = 'data'>
-                <h3>Todays Cases: <span>{data.todayCases.toLocaleString()} |</span> </h3>
-                <h3>Deaths: <span>{data.todayDeaths.toLocaleString()}</span></h3>
+                <h3>Todays Cases: <span>{getData('todayCases')} |</span> </h3>
+                <h3>Deaths: <span>{getData('todayDeaths')}</span></h3>
               </div>
               <div className = 'data'>
-                <h3>Active Cases: <span>{data.active !== null ? data.active.toLocaleString() : '--'} |</span> </h3>
-                <h3>Critical: <span>{data.critical.toLocaleString()}</span></h3>
+                <h3>Active Cases: <span>{getData('active')} |</span> </h3>
+                <h3>Critical: <span>{getData('critical')}</span></h3>
               </div>
               <div className = 'data'>
-                <h3>Total Cases: <span>{data.cases.toLocaleString()} |</span></h3> 
-                <h3>Deaths: <span>{data.deaths.toLocaleString()}</span></h3>
+                <h3>Total Cases: <span>{getData('cases')} |</span></h3> 
+                <h3>Deaths: <span>{getData('deaths')}</span></h3>
             </div>
-        </ListContainer>            
-        )}
+          </ListContainer>    
+          )
+        }        
+      )}
     </Fragment>
     )
 }
